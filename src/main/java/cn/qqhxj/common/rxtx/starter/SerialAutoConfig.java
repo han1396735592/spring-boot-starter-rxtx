13170bf74c0b1186b2aa7ebf4ce841116496629a
han1396735592 <1396735592@qq.com>
2019-11-15 11:31:40 +0800

    1.修改了pom配置

--- src/main/java/cn/qqhxj/common/rxtx/starter/SerialAutoConfig.java
+++ src/main/java/cn/qqhxj/common/rxtx/starter/SerialAutoConfig.java
@@ -1,6 +1,7 @@
 package cn.qqhxj.common.rxtx.starter;
 
 import cn.qqhxj.common.rxtx.DefaultSerialDataListener;
+import cn.qqhxj.common.rxtx.HexUtil;
 import cn.qqhxj.common.rxtx.parse.StringSerialDataParser;
 import cn.qqhxj.common.rxtx.processor.SerialByteDataProcessor;
 import cn.qqhxj.common.rxtx.reader.SerialReader;
@@ -24,9 +25,6 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(SerialPort.class)
 public class SerialAutoConfig {
 
-    @Autowired
-    private SerialPortProperties serialPortProperties;
-
     @Bean
     @ConditionalOnMissingBean
     public SerialPortEventListener serialPortEventListener() {
@@ -39,34 +37,15 @@ public class SerialAutoConfig {
     @Bean
     @ConditionalOnMissingBean
     public SerialReader serialReader() {
-        SerialReader serialReader = null;
-        serialReader = new VariableLengthSerialReader('{', '}');
+        SerialReader serialReader = new VariableLengthSerialReader('{', '}');
         log.debug("config SerialReader ={}", serialReader);
         return serialReader;
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public SerialByteDataProcessor serialByteDataProcesser() {
-        return new SerialByteDataProcessor() {
-            @Override
-            public void process(byte[] bytes) {
-                log.debug("Received Data:{}", bytesToHexString(bytes));
-            }
-
-            public String bytesToHexString(byte[] bArr) {
-                StringBuffer sb = new StringBuffer(bArr.length);
-                String sTmp;
-                for (int i = 0; i < bArr.length; i++) {
-                    sTmp = Integer.toHexString(0xFF & bArr[i]);
-                    if (sTmp.length() < 2) {
-                        sb.append(0);
-                    }
-                    sb.append(sTmp.toUpperCase() + "  ");
-                }
-                return sb.toString();
-            }
-        };
+    public SerialByteDataProcessor serialByteDataProcessor() {
+        return bytes -> log.debug("Received Data:{}", HexUtil.bytesToHexString(bytes));
     }
 
     @Bean
