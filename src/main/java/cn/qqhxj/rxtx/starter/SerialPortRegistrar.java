147aca7fdd571cd12288d849f9e95fa50e58c8fa
han1396735592 <1396735592@qq.com>
2022-06-29 16:49:53 +0800

    update readme.md add log

--- src/main/java/cn/qqhxj/rxtx/starter/SerialPortRegistrar.java
+++ src/main/java/cn/qqhxj/rxtx/starter/SerialPortRegistrar.java
@@ -5,8 +5,6 @@ import cn.qqhxj.rxtx.SerialUtils;
 import cn.qqhxj.rxtx.starter.annotation.EnableSerialPort;
 import gnu.io.SerialPort;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
